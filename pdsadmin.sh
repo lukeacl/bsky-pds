a96479dd551c236fa2b6f8cd738bc2c345319c5f
lukeacl <hello@lukeacl.com>
2025-08-31 16:38:34 +1000

    Update pdsadmin.sh - ensure jq

--- pdsadmin.sh
+++ pdsadmin.sh
@@ -3,6 +3,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if ! command -v jq &> /dev/null; then
+    echo "Error: jq could not be found. Please install it." >&2
+    exit 1
+fi
+
 BASEDIR=$(dirname "$0")
 
 # Command to run.
